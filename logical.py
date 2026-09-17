974a4b77d2fbaf2a543938b6b525f58ee8ca7e04
harshananduri157 <harshananduri2418@gmail.com>
2026-09-17 23:09:53 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,14 @@
+a=5
+a > 3 and a < 10
+print("result of",a,"> 3 and <",10,"is",a > 3 and a < 10)
+a > 3 or a < 4
+print("result of",a,"> 3 or <",4,"is",a > 3 or a < 4)
+not (a > 3 and a < 10)
+print("result of not (",a,"> 3 and <",10,") is",not (a > 3 and a < 10))
+a = 1
+a > 3 and a < 10
+print("result of",a,"> 3 and <",10,"is",a > 3 and a < 10)
+a > 3 or a < 4
+print("result of",a,"> 3 or <",4,"is",a > 3 or a < 4)
+a > 3 and a < 10
+print("result of not (",a,"> 3 and <",10,") is",not (a > 3 and a < 10)  )
