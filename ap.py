15a8db50f54510e618a20fc40eb543a65d1ab53d
harshananduri157 <harshananduri2418@gmail.com>
2026-09-17 23:08:06 +0530

    Add files via upload

---
+++ ap.py
@@ -0,0 +1,9 @@
+a=7
+b=5
+print("sum of a and b is :",a+b)
+print("difference of a and b",a-b)
+print("multiplication of a and b",a*b)
+print("modulus of a and b ",a%b)
+print("division of a and b",a/b)
+print("floor division of a and b",a//b)
+print("exponent of a and b",a**b)
---
+++ memembership_operators.py
@@ -0,0 +1,7 @@
+"h" in "harsha"  # True
+print("result:", "h" in "harsha")
+
+"g" in  "zebra"  # false
+print("result:", "g" in "zebra")
+
+
