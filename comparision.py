6603d2fc96383d0bca8a4ff20584548fd397ac45
harshananduri157 <harshananduri2418@gmail.com>
2026-09-17 21:51:02 +0530

    Add files via upload

---
+++ comparision.py
@@ -0,0 +1,9 @@
+a=7
+b=3
+result = a > b
+print("result of",a,">",b,"is",result)
+print("result of",a,"<",b,"is",a<b)
+print("result of",a,">=",b,"is",a>=b)
+print("result of",a,"<=",b,"is",a<=b)
+print("result of",a,"==",b,"is",a==b)
+print("result of",a,"!=",b,"is",a!=b)
---
+++ identity.py
@@ -0,0 +1,12 @@
+a=[1,2,3]
+b=a
+print("result of",a,"is",b,"is",a is b)
+a=[1,2,3]
+b=[1,2,3]
+print("result of",a,"is",b,"is",a is b)
+a=[4,5,6]
+b=[4,5,6]
+print("result of",a,"is",b,"is",a is b)
+a=[4,5,6]
+b=a
+print("result of",a,"is",b,"is",a is b)
